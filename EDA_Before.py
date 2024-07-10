01f2b4e5cc3888c0bcbf38b4e008abc3b361a62f
Santhosh-Analytics <sansuganyas@outlook.com>
2024-07-10 18:39:12 +0530

    Categorical Encoding

---
+++ EDA_Before.py
@@ -0,0 +1,99 @@
+
+def univar_num(df, col):
+    plt.figure(figsize=(16, 8))
+    df = df.dropna(subset=[col])
+
+
+    mean=df[col].mean()
+    median=df[col].median()
+    mode=df[col].mode()[0]
+    sns.set_style({'axes.facecolor': 'white','axes.edgecolor': 'white'})
+    plt.subplot(2, 3, 1)
+    sns.histplot(data=df, x=col, kde=True, bins=50,color='maroon',fill=True)
+    plt.title(f'Histogram for {col}')
+    plt.axvline(mean, color='maroon', label='Mean')
+    plt.axvline(median, color='black', label='Median')
+    plt.axvline(mode, color='darkgreen', label='Mode')
+    sns.set_theme(context='notebook', style='white', palette='dark', font='sans-serif', font_scale=1, color_codes=True, rc=None)
+
+    
+    plt.legend()
+    plt.tight_layout()
+    
+    plt.subplot(2, 3, 2)
+    sns.boxplot(data=df, x=col, color='lightgrey',legend=True)
+    plt.xticks(rotation=45)
+    plt.title(f'Box Plot for {col}')
+    plt.axvline(mean, color='maroon', label='Mean')
+    plt.axvline(median, color='black', label='Median')
+    plt.axvline(mode, color='darkgreen', label='Mode')
+    plt.legend()
+    plt.tight_layout()
+
+    
+    plt.subplot(2, 3, 3)
+    stats.probplot(df[col], dist="norm", plot=plt)
+    plt.gca().get_lines()[1].set_color('maroon')
+    plt.gca().get_lines()[0].set_color('darkgreen')
+    plt.title(f'QQ Plot for {col}')
+
+    plt.tight_layout()
+    
+    plt.tight_layout()
+    plt.show()
+
+
+# In[33]:
+
+
+def kurtosis_color(kurt_val):
+    if -1 <= kurt_val <= 1:
+        kurt_color = color_good  # Dark Green for near-zero kurtosis (good)
+    elif 1 < kurt_val <= 3 or -3 <= kurt_val < -1:
+        kurt_color = color_okay  # Yellow for moderate kurtosis
+    else:  # kurt_val > 3 or kurt_val < -3
+        kurt_color = color_bad  # Maroon for extreme kurtosis
+    return kurt_color
+
+def skewness_color(skew_val):
+    if -0.5 <= skew_val <= 0.5:
+        color = color_good  # Dark Green for near-zero skew
+    elif 0 < skew_val <= 0.5 or -0.5 < skew_val < 0:
+        color = color_okay  # Light Green for slightly positive or slightly negative skew
+    else:  # skew_val > 0.5 or skew_val < -0.5
+        color = color_bad  # Maroon for significant skew
+    return color
+
+
+
+def analyze_categorical_relationship(df, continuous_var, categorical_var):
+    # Perform one-way ANOVA
+    model = ols(f'{continuous_var} ~ C({categorical_var})', data=df).fit()
+    anova_table = sm.stats.anova_lm(model, typ=2)
+    
+    # Calculate eta-squared (effect size)
+    ssr = anova_table.loc['C(' + categorical_var + ')', 'sum_sq']
+    sst = ssr + anova_table.loc['Residual', 'sum_sq']
+    eta_squared = ssr / sst
+    
+    # Convert eta-squared to percentage
+    significance_percentage = eta_squared * 100
+    
+    # Determine if there's a significant relationship
+    p_value = anova_table.loc['C(' + categorical_var + ')', 'PR(>F)']
+    alpha = 0.05  # significance level
+    is_significant = p_value < alpha
+    
+    print(f"Relationship between {continuous_var} and {categorical_var}:")
+    print(f"ANOVA: F-statistic = {anova_table.loc['C(' + categorical_var + ')', 'F']:.4f}, p-value = {p_value:.4f}")
+    print(f"{categorical_var} has {significance_percentage:.2f}% significance with {continuous_var}")
+    print(f"There is {'a' if is_significant else 'no'} statistically significant relationship between {categorical_var} and {continuous_var}.")
+    print()
+    
+# Analyze relationships
+
+def boxcox_transform(x):
+    transformed_data,lmbda = boxcox(x)
+    return transformed_data,lmbda
+
+
